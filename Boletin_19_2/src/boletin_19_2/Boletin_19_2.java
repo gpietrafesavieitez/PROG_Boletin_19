d5118b9951a27bab60759ba3d4e213d15c54f6f2
gpietrafesavieitez <gpietrafesavieitez@danielcastelao.org>
2019-01-30 12:37:47 +0100

    Listo

--- Boletin_19_2/src/boletin_19_2/Boletin_19_2.java
+++ Boletin_19_2/src/boletin_19_2/Boletin_19_2.java
@@ -16,7 +16,7 @@ public class Boletin_19_2 {
      */
     public static void main(String[] args) {
         int notas[] = new int[]{5,7,9,6,3,1,8,2,5,9};
-        int c_aprobados, c_suspensos, c_alta;
+        int c_aprobados, c_suspensos, c_alta, c_media = 0;
         c_aprobados = c_suspensos = c_alta = 0; //  Seteo contadores a 0 
         for(int i = 0; i < notas.length; i ++){ //  Cursor recorre array
             if(notas[i] >= 5){ //   Si la nota es un aprobado
@@ -27,12 +27,12 @@ public class Boletin_19_2 {
             if(notas[i] > c_alta){ //   Si la nota actual es > que la ultima mas alta
                 c_alta = notas[i]; //   La recojo como la mas alta hasta que no haya una superior
             }
+            c_media += notas[i];
         }
         //  Output
         System.out.println("-\tNº de Aprobados: " + c_aprobados);
         System.out.println("-\tNº de Suspensos: " + c_suspensos);
+        System.out.println("-\tNota media de la clase: " + c_media/notas.length);
         System.out.println("-\tNota más alta: " + c_alta);
-        
     }
-    
 }
